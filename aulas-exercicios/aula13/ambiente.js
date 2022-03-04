919d39b243cdadff3cc2c139bb32aa527f2d8f8f
Jeferson Paduli <84051934+jefersonpaduli@users.noreply.github.com>
2022-03-04 10:31:35 -0300

    Update ambiente.js

--- aulas-exercicios/aula13/ambiente.js
+++ aulas-exercicios/aula13/ambiente.js
@@ -1,5 +1,13 @@
+// estrtura de repetição com teste lógico no final
 var c = 1
-while (c <= 8) {
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 6)
+
+// estrutura de repetição teste lógico no início
+var c = 1
+while (c <= 6) {
     console.log(`passo ${c}`)
     c++
 }
